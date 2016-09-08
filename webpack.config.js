4af5547db5df7c0fa208afeededaa655e179a730
nateph <nbp615@gmail.com>
2016-09-08 10:08:50 -0600

    first commit of project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+  // plugins:[
+  //   new webpack.DefinePlugin({                       //FOR PRODUCTION
+  //     'process.env':{
+  //       'NODE_ENV': JSON.stringify('production')
+  //     }
+  //   }),
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     compress:{
+  //       warnings: true
+  //     }
+  //   })
+  // ]
+};
