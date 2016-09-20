c678c4fc7901b0d89b342f9df266862ec382c372
nateph <nbp615@gmail.com>
2016-09-19 18:39:34 -0600

    added some css, and started signin

---
+++ src/components/auth/signin.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {reduxForm} from 'redux-form'
+
+class Signin extends Component {
+  handleFormSubmit({username, password}) {
+    console.log(username, password);
+  }
+  //Need to do something to log user in
+
+  render() {
+
+    const {handleSubmit, fields: {username, password}} = this.props;
+
+    return (
+      <form className="form-group" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <fieldset>
+          <label><span><i className="fa fa-user" aria-hidden="true"></i></span> Username:</label>
+          <input {...username} type="text" className="form-control"/>
+        </fieldset>
+        <fieldset>
+          <label><span><i className="fa fa-key" aria-hidden="true"></i></span> Password:</label>
+          <input {...password} type="password" className="form-control"/>
+        </fieldset>
+        <button action="submit" className="btn btn-primary">Sign In</button>
+    </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'signin',
+  fields: ['username', 'password']
+})(Signin);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 import App from './components/app';
+import Signin from './components/auth/signin';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={Signin}/>
+      </Route>
+    </Router>
   </Provider>
   , document.querySelector('.container'));
